5cbabd24591f1b9243afa5a8bdfefb51aedb6277
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-15 18:00:45 +0300

    feat: add adaptive for the page

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
 "use strict";
 
 /*-------------------------------------------------------------*/
-/*-----Burger--------------------------------------------------*/
-/*-------------------------------------------------------------*/
-const iconMenu = document.querySelector(".menu__icon");
-const menuBody = document.querySelector(".menu__body");
-
-if (iconMenu) {
-  iconMenu.addEventListener("click", (event) => {
-    document.body.classList.toggle("_lock");
-    iconMenu.classList.toggle("_active");
-    menuBody.classList.toggle("_active");
-  });
-}
-
-const menuLinks = document.querySelectorAll(".menu__link");
-menuLinks.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (iconMenu.classList.contains("_active")) {
-      document.body.classList.remove("_lock");
-      iconMenu.classList.remove("_active");
-      menuBody.classList.remove("_active");
-    }
-  });
-});
-
-/*-------------------------------------------------------------*/
 /*-----Equation------------------------------------------------*/
 /*-------------------------------------------------------------*/
 
